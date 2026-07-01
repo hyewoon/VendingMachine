649abd019120b502c4c66c3aeed3ecdeb5994706
hyewoon <likethebegin@naver.com>
2026-07-02 00:07:33 +0900

    refacotor: VendingMachineView에 유효성검사 코드 분리 및 수정

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         VendingMachineView vendingMachineView = new VendingMachineView();
-        vendingMachineView.makeOrder();
+        vendingMachineView.startVendingMachine();
     }
 }
--- src/VendingMachineView.java
+++ src/VendingMachineView.java
@@ -7,10 +7,81 @@ public class VendingMachineView {
     Scanner scanner = new Scanner(System.in);
     VendingMachine vendingMachine = new VendingMachine();
 
+    public void startVendingMachine() {
+        while (true) {
+            System.out.println("자판기 프로그램");
+            System.out.println("[0].종료  [1].주문하기");
+            System.out.println("탭을 선택하세요.");
+
+            int inputValue = checkInputValidation();
+
+            if (inputValue == 1) {
+                makeOrder();
+                continue;
+            } else if (inputValue == 0) break;
+            else System.out.println("다시 입력해 주세요.");
+        }
+    }
+
+
+    //입력값이 존재하는 지, 그 입력값이 형식(숫자)맞는지 확인 용
+    private int checkInputValidation() {
+
+        while (true) {
+            String inputValue = scanner.nextLine();
+            if (inputValue.trim().isEmpty()) {
+                System.out.println("입력값이 없습니다.");
+                System.out.println("다시 입력해 주세요.");
+                continue;
+            }
+
+            try {
+                return Integer.parseInt(inputValue.trim());
+            } catch (NumberFormatException e) {
+                System.out.println("숫자를 입력해 주세요.");
+            }
+        }
+    }
+
+    public void makeOrder() {
+        int menu = 0;
+        int money = 0;
+
+            showMenu();
+            menu = chooseMenu();
+            money = insertMoney();
+
+            PaymentResult result = vendingMachine.takeOrder(menu, money);
+            showResult(result);
+
+    }
+
+    private int chooseMenu() {
+            int menu = 0;
+            System.out.println("음료를 선택하세요.");
+            menu = checkInputValidation();
+
+            //menu번호 가져오기
+            return menu;
+
+    }
+
+    private int insertMoney() {
+        while (true) {
+            System.out.println("금액을 입력하세요.");
+            int money = checkInputValidation();
+            if ( money < 0) {
+                System.out.println("입금액이 없습니다.");
+                continue;
+            }
+            return money;
+        }
+
+    }
+
     private void showMenu() {
 
-        //getMemu()를 호출해서 그 return값을 받아서 화면에 출력함
-        System.out.println("============== 메뉴 ==========");
+        System.out.println("============== 메뉴창 ==========");
         Map<Integer, Product> menu = vendingMachine.getMenu();
         for (Map.Entry<Integer, Product> entry : menu.entrySet()) {
             System.out.println(entry.getKey() + "."
@@ -18,27 +89,6 @@ public class VendingMachineView {
                     + entry.getValue().getProductPrice() + "원");
         }
         System.out.println("===============================");
-    }
-
-    public void makeOrder() {
-        int menu = 0;
-        int money = 0;
-        int input = 0;
-
-        while (true) {
-            showMenu();
-            //입력값 받기
-            menu = checkValidation(1, 4, "번호를 선택하세요");
-
-            money = checkValidation(0, 1000000, "금액을 투입하세요");
-
-            PaymentResult result = vendingMachine.takeOrder(menu, money);
-            showResult(result);
-
-            input = checkValidation(1, 2, "주문을 계속하시겠습니까? 1.YES 2.NO");
-            if (input == 2) break;
-
-        }
     }
 
 
@@ -65,7 +115,9 @@ public class VendingMachineView {
             } catch (NumberFormatException e) {
                 System.out.println(" 숫자만 입력해주세요.");
             }
+            scanner.close();
         }
+
     }
 
     private void showResult(PaymentResult result) {
@@ -77,6 +129,4 @@ public class VendingMachineView {
             System.out.println("환불액:" + result.change + "원");
         }
     }
-
-
 }
